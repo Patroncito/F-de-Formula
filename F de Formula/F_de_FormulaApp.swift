d6e6c249c088a85bbae06020b0e54321fe534ae0
Alfonso Patron <c9alfonsopatron@hotmail.com>
2024-01-30 22:10:58 -0700

    Initial Commit

---
+++ F de Formula/F_de_FormulaApp.swift
@@ -0,0 +1,17 @@
+//
+//  F_de_FormulaApp.swift
+//  F de Formula
+//
+//  Created by Alfonso Patron on 30/01/24.
+//
+
+import SwiftUI
+
+@main
+struct F_de_FormulaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
